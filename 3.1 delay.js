1cdfae61be6adf273be241d326d76e0ed300eb7f
aaront999 <76186823+aaront999@users.noreply.github.com>
2025-01-08 12:51:06 -0800

    3.1 delay.js

---
+++ 3.1 delay.js
@@ -0,0 +1,12 @@
+function delay(callback, waitTimeInMilli) {
+  return function() {
+    return setTimeout(callback, waitTimeInMilli);
+  }
+}
+
+// UNCOMMENT THE CODE BELOW TO TEST DELAY
+let count = 0;
+const delayedFunc = delay(() => count++, 1000);
+delayedFunc();
+console.log(count); 												 // should print '0'
+setTimeout(() => console.log(count), 1000); // should print '1' after 1 second
